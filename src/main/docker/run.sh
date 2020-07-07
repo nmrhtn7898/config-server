714a99ff48c74929a671a2e3556e07ece358b9a1
syj <nmrhtn7898@naver.com>
2020-07-07 20:35:16 +0900

    add docker files

---
+++ src/main/docker/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "*******************************************"
+echo "run spring cloud config server"
+echo $RUN_MESSAGE
+echo "*******************************************"
+java -jar app.jar
